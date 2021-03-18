b8d124646782f777f27908836b8b9f3ec1fcfa5d
DanielS <daessar12@gmail.com>
2021-03-18 12:47:52 -0500

    Agrego el controlador HomeController y en la carpeta modelo creo la clase usuario con sus respetivos getters and setters

---
+++ Queries/Insertar_datos_prueba.sql
@@ -0,0 +1,20 @@
+INSERT INTO es_usuarios(
+usu_documento,
+usu_tipodoc,
+usu_nombre,
+usu_celular,
+usu_email,
+usu_genero,
+usu_aprendiz,
+usu_egresado,
+usu_areaformacion,
+usu_fechaegresado,
+usu_direccion,
+usu_barrio,
+usu_ciudad,
+usu_departamento,
+usu_fecharegistro)
+VALUES(
+2345678,'CC','Camilo',30012,'camilo@camilo.com','M','No','Si','ADSI',GETDATE(),'Cra 22 #34-50','El sol','Manizales','Caldas',GETDATE())
+
+SELECT * FROM es_usuarios
---
+++ Siena/Models/Usuario.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Siena.Models
+{
+    public class Usuario
+    {
+        [Required(ErrorMessage = "El numero de documento es obligatorio")]
+        [MaxLength(10, ErrorMessage = "El numero de documento excede el numero maximo de caracteres (10)")]
+        public int Documento { get; set; }
+
+        [Required(ErrorMessage = "El tipo de documento es obligatorio")]
+        public string TipoDocumento { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [MaxLength(10, ErrorMessage = "El numero de documento excede el numero maximo de caracteres (30)")]
+        public string Nombre { get; set; }
+
+        [MaxLength(10, ErrorMessage = "El numero de celular excede el numero maximo de caracteres (10)")]
+        public int Celular { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [EmailAddress(ErrorMessage = "Debe ingresar un mail válido")]
+        [MaxLength(10, ErrorMessage = "El numero de documento excede el numero maximo de caracteres (30)")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "El genero es obligatorio")]
+        public string Genero { get; set; }
+
+        [Required(ErrorMessage = "La selección es obligatorio")]
+        public string Aprendiz { get; set; }
+
+        [Required(ErrorMessage = "La selección es obligatorio")]
+        public string Egresado { get; set; }
+
+        [Required(ErrorMessage = "El nombre de la formación es obligatorio")]
+        [MaxLength(30, ErrorMessage = "El nombre de la formación excede el numero maximo de caracteres (30)")]
+        public string AreaFormacion { get; set; }
+
+        public string FechaEgresado{ get; set; }
+
+        [Required(ErrorMessage = "La direccion es obligatorio")]
+        [MaxLength(30, ErrorMessage = "La direccion excede el numero maximo de caracteres (30)")]
+        public string Direccion { get; set; }
+
+        [Required(ErrorMessage = "El barrio es obligatorio")]
+        [MaxLength(30, ErrorMessage = "El barrio excede el numero maximo de caracteres (30)")]
+        public string Barrio { get; set; }
+
+        [Required(ErrorMessage = "La ciudad es obligatoria")]
+        [MaxLength(30, ErrorMessage = "El nombre de la ciudad excede el numero maximo de caracteres (30)")]
+        public string Ciudad { get; set; }
+
+        [Required(ErrorMessage = "El departamento es obligatorio")]
+        [MaxLength(30, ErrorMessage = "El nombre del departamento excede el numero maximo de caracteres (30)")]
+        public string Departamento { get; set; }
+
+        public string FechaRegistro { get; set; }
+
+    }
+}
